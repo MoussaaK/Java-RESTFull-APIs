ab002c9fb8503acf84a8756993d29e0e60b9c7aa
Mousse <moussakonate27@gmail.com>
2019-01-26 21:44:31 +0100

    App Class added

---
+++ src/main/java/org/konate/tpjavajee/rest/CommuneApplication.java
@@ -0,0 +1,9 @@
+package org.konate.tpjavajee.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class CommuneApplication extends Application {
+
+}
