18a324602312d1207afa94494c948138e7986974
Mousse <moussakonate27@gmail.com>
2019-02-13 23:25:04 +0100

    Updated Ctrls

--- WebContent/app.js
+++ WebContent/app.js
@@ -1,89 +1,83 @@
 var app = angular.module("CityApp", []);
 
 function getMaires($scope) {
+	$scope.maires = [];
+	$scope.search = function () {
+		var resource_url = "http://localhost:8080/tp-jee-glassfish/rest/maire/all";
+		$.ajax({
+			contentType: "",
+			async: false,
+			method: "GET",
+			url: resource_url,
+			dataType: "json",
+			success: function(data) {
+				console.log(data);
+				data[0].forEach(function(maire) {
+					$scope.maires.push({
+						ID : maire[0],
+						Nom : maire[1],
+						Prenom : maire[2]
+					});
+				}, this);
 
-  $scope.maires = [];
-  $scope.search = function () {
-    var city = $scope.cityName;
-    var tmpUrl = "http://localhost:8080/tp-jee-glassfish/rest/maire/all";
-    $.ajax({
-      contentType: "",
-      async: false,
-      method: "GET",
-      url: tmpUrl,
-      dataType: "json",
-      success: function(data) {
-        console.log(data);
-        data[0].forEach(function(maire) {
-          
-          $scope.maires.push({
-        	  ID : maire[0],
-	          Nom : maire[1],
-	          Prenom : maire[2]
-          });
-        }, this);
-        
-        console.log($scope.maires);
-      }
-    });
-  }
+				console.log($scope.maires);
+			}
+		});
+	}
 }
 app.controller("maireCtrl", getMaires);
 
 function getCommunes($scope) {
+	$scope.communes = [];
+	$scope.search = function () {
+		var resource_url = "http://localhost:8080/tp-jee-glassfish/rest/commune/all";
+		$.ajax({
+			contentType: "",
+			async: false,
+			method: "GET",
+			url: resource_url,
+			dataType: "json",
+			success: function(data) {
+				console.log(data);
+				data[0].forEach(function(commune) {
+					$scope.communes.push({
+						"ID" : commune[0],
+						"Nom" : commune[1],
+						"Maire" : commune[2]
+					});
+				}, this);
 
-	  $scope.communes = [];
-	  $scope.search = function () {
-	    var tmpUrl = "http://localhost:8080/tp-jee-glassfish/rest/commune/all";
-	    $.ajax({
-	      contentType: "",
-	      async: false,
-	      method: "GET",
-	      url: tmpUrl,
-	      dataType: "json",
-	      success: function(data) {
-	        console.log(data);
-	        data[0].forEach(function(commune) {
-	          $scope.communes.push({
-	            "ID" : commune[0],
-	            "Nom" : commune[1],
-	            "Maire" : commune[2]
-	          });
-	        }, this);
-	        
-	        console.log($scope.communes);
-	      }
-	    });
-	  }
+				console.log($scope.communes);
+			}
+		});
 	}
+}
 app.controller("communeCtrl", getCommunes);
 
 function getDepartements($scope) {
+	$scope.dpts = [];
+	$scope.search = function () {
+		var city = $scope.cityName;
+		var resource_url = "http://localhost:8080/tp-jee-glassfish/rest/departement/all";
+		$.ajax({
+			contentType: "",
+			async: false,
+			method: "GET",
+			url: resource_url,
+			dataType: "json",
+			success: function(data) {
+				console.log(data);
+				data[0].forEach(function(dpt) {
 
-	  $scope.dpts = [];
-	  $scope.search = function () {
-	    var city = $scope.cityName;
-	    var tmpUrl = "http://localhost:8080/tp-jee-glassfish/rest/departement/all";
-	    $.ajax({
-	      contentType: "",
-	      async: false,
-	      method: "GET",
-	      url: tmpUrl,
-	      dataType: "json",
-	      success: function(data) {
-	        console.log(data);
-	        data[0].forEach(function(dpt) {
-	          
-	          $scope.dpts.push({
-	        	  "ID" : dpt[0],
-	        	  "Nom" : dpt[1],
-	        	  "Code_Postale" : dpt[2]
-	          });
-	        }, this);
-	        
-	        console.log($scope.dpts);
-	      }
-	    });
-	  }
+					$scope.dpts.push({
+						"ID" : dpt[0],
+						"Nom" : dpt[1],
+						"Code_Postale" : dpt[2]
+					});
+				}, this);
+				console.log($scope.dpts);
+			}
+		});
 	}
+}
 app.controller("departementCtrl", getDepartements);
